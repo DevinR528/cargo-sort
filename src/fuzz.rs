f9a61daab09e8cd9546a5eb5f0e52c19fdd37427
ssrlive <30760636+ssrlive@users.noreply.github.com>
2024-10-20 11:03:40 +0800

    Upgrade toml-edit crate

--- src/fuzz.rs
+++ src/fuzz.rs
@@ -4,7 +4,7 @@ mod fmt;
 mod sort;
 
 use fmt::Config;
-use toml_edit::Document;
+use toml_edit::DocumentMut;
 
 // cargo afl build --bin=fuzz --features=fuzz
 // cargo afl fuzz -i examp/ -o target/cargo-sort-fuzz -- target/debug/fuzz
@@ -12,7 +12,7 @@ fn main() {
     fuzz!(|data: &[u8]| {
         if let Ok(s) = std::str::from_utf8(data) {
             let s = s.replace("\r", "");
-            if s.parse::<Document>().is_ok() {
+            if s.parse::<DocumentMut>().is_ok() {
                 let mut toml = sort::sort_toml(
                     &s,
                     sort::MATCHER,
@@ -27,7 +27,7 @@ fn main() {
                 );
                 fmt::fmt_toml(&mut toml, &Config::new());
                 let s = toml.to_string();
-                assert!(s.parse::<Document>().is_ok())
+                assert!(s.parse::<DocumentMut>().is_ok())
             }
         }
     });
